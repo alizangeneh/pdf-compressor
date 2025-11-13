41626a4d8da1a23f046f977a75c7991d3c6a519f
Ali Zangeneh <engineer.zangeneh@gmail.com>
2025-11-13 14:26:14 +0330

    Fix cross-platform build workflow and macOS py2app setup

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup
 
 APP = ['main.py']
+DATA_FILES = []
 OPTIONS = {
     'argv_emulation': True,
-    'packages': ['fitz', 'PIL', 'tkinterdnd2', 'ghostscript'],
+    'packages': ['PyMuPDF', 'Pillow', 'tkinterdnd2', 'ghostscript'],
 }
 
 setup(
     app=APP,
+    data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
